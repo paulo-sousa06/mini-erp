da6a4dd40ff0b509d55c184e5b5f9d374cc9285f
Paulo Sousa <desousacordeiroricardopaulo@gmail.com>
2026-07-19 21:48:25 -0300

    Atualiza tela inicial do sistema

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mini Erp</title>
+</head>
+<body>
+
+    <h1>Bem-vindo ao Mini Erp</h1>
+
+    <p>Sistema desenvolvido em Laravel</p>
+
+</body>
+</html>
